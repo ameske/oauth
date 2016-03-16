21464d16487c0809ff43f3739239c7cde6fc7598
Kyle Ames <KyleAmes13@gmail.com>
2016-03-16 13:34:14 -0400

    Separated OAuth code into a separate external package

---
+++ oauth.go
@@ -0,0 +1,194 @@
+package oauth
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"sort"
+	"strconv"
+	"time"
+)
+
+type Config struct {
+	ConsumerKey       string `json:"consumerKey"`
+	ConsumerSecret    string `json:"consumerSecret"`
+	AccessToken       string `json:"accessToken"`
+	AccessTokenSecret string `json:"accessTokenSecret"`
+	Version           string `json:"version"`
+}
+
+func NewConfig(consumerKey, consumerSecret, accessToken, accessTokenSecret, version string) Config {
+	return Config{
+		ConsumerKey:       consumerKey,
+		ConsumerSecret:    consumerSecret,
+		AccessToken:       accessToken,
+		AccessTokenSecret: accessTokenSecret,
+		Version:           version,
+	}
+}
+
+func (config Config) header() *oauthHeader {
+	return &oauthHeader{
+		ConsumerKey:     config.ConsumerKey,
+		Nonce:           oauthNonce(),
+		SignatureMethod: "HMAC-SHA1",
+		Timestamp:       strconv.Itoa(int(time.Now().Unix())),
+		AccessToken:     config.AccessToken,
+		Version:         "1.0",
+	}
+}
+
+func (config Config) HashKey() string {
+	return config.ConsumerSecret + "&" + config.AccessTokenSecret
+}
+
+var headerFormat = `OAuth %s="%s", %s="%s", %s="%s", %s="%s", %s="%s", %s="%s", %s="%s"`
+
+type oauthHeader struct {
+	ConsumerKey     string
+	Nonce           string
+	Signature       string
+	SignatureMethod string
+	Timestamp       string
+	AccessToken     string
+	Version         string
+}
+
+func (h *oauthHeader) Map() map[string]string {
+	return map[string]string{
+		"oauth_consumer_key":     h.ConsumerKey,
+		"oauth_nonce":            h.Nonce,
+		"oauth_signature_method": h.SignatureMethod,
+		"oauth_timestamp":        h.Timestamp,
+		"oauth_token":            h.AccessToken,
+		"oauth_version":          h.Version,
+	}
+}
+
+func (h *oauthHeader) String() string {
+	return fmt.Sprintf(headerFormat,
+		"oauth_consumer_key",
+		percentEncode(h.ConsumerKey),
+		"oauth_nonce",
+		percentEncode(h.Nonce),
+		"oauth_signature",
+		percentEncode(h.Signature),
+		"oauth_signature_method",
+		percentEncode(h.SignatureMethod),
+		"oauth_timestamp",
+		percentEncode(h.Timestamp),
+		"oauth_token",
+		percentEncode(h.AccessToken),
+		"oauth_version",
+		percentEncode(h.Version))
+}
+
+func (config Config) Sign(req *http.Request, header *oauthHeader) error {
+	params, err := sortedOauthParameters(req, header)
+	if err != nil {
+		return err
+	}
+
+	s := oauthSignatureString(req, params)
+
+	header.Signature = oauthSign(s, config.HashKey())
+	req.Header.Add("Authorization", header.String())
+
+	return nil
+}
+
+func oauthSign(s string, key string) string {
+	sha1Hmac := hmac.New(sha1.New, []byte(key))
+	sha1Hmac.Write([]byte(s))
+
+	hash := sha1Hmac.Sum(nil)
+
+	return base64.StdEncoding.EncodeToString(hash)
+}
+
+func oauthNonce() string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	n := r.Int31()
+
+	var b bytes.Buffer
+	binary.Write(&b, binary.LittleEndian, &n)
+	return base64.StdEncoding.EncodeToString(b.Bytes())
+}
+
+func oauthSignatureString(req *http.Request, params []oauthParam) string {
+	var signatureString bytes.Buffer
+
+	// Method
+	signatureString.WriteString(req.Method)
+	signatureString.WriteByte('&')
+
+	// URL
+	signatureString.WriteString(percentEncode(req.URL.Scheme + "://" + req.URL.Host + req.URL.Path))
+	signatureString.WriteByte('&')
+
+	// Parm String
+	var buf bytes.Buffer
+	for i, p := range params {
+		buf.WriteString(p.key)
+		buf.WriteString("=")
+		buf.WriteString(p.value)
+		if i+1 < len(params) {
+			buf.WriteByte('&')
+		}
+	}
+	signatureString.WriteString(percentEncode(buf.String()))
+
+	return signatureString.String()
+}
+
+type oauthParam struct{ key, value string }
+
+type paramList []oauthParam
+
+func (p paramList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+
+func (p paramList) Len() int { return len(p) }
+
+func (p paramList) Less(i, j int) bool { return p[i].key < p[j].key }
+
+func sortedOauthParameters(req *http.Request, h *oauthHeader) ([]oauthParam, error) {
+	// Gather the needed components for the signing
+	header := h.Map()
+	query := req.URL.Query()
+
+	var body url.Values
+
+	if req.Body != nil {
+		var err error
+		bodyBytes, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			return nil, err
+		}
+		body, err = url.ParseQuery(string(bodyBytes))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	params := make([]oauthParam, 0, len(header)+len(query)+len(body))
+	for k, v := range body {
+		params = append(params, oauthParam{key: percentEncode(k), value: percentEncode(v[0])})
+	}
+	for k, v := range query {
+		params = append(params, oauthParam{key: percentEncode(k), value: percentEncode(v[0])})
+	}
+	for k, v := range header {
+		params = append(params, oauthParam{key: percentEncode(k), value: percentEncode(v)})
+	}
+
+	sort.Sort(paramList(params))
+
+	return params, nil
+}
---
+++ oauth_test.go
@@ -0,0 +1,69 @@
+package oauth
+
+import (
+	"bytes"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+var expectedHashKey = "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw&LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE"
+var expectedSigString = "POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521"
+var expectedSignature = "tnnArxj06cWHq44gCs1OSKk/jLY="
+
+var testRequest *http.Request
+
+var testConfig = OAuthConfig{
+	ConsumerKey:       "xvz1evFS4wEEPTGEFPHBog",
+	ConsumerSecret:    "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw",
+	AccessToken:       "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb",
+	AccessTokenSecret: "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE",
+	Version:           "1.0",
+}
+
+var testHeader = oauthHeader{
+	ConsumerKey:     "xvz1evFS4wEEPTGEFPHBog",
+	Nonce:           "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg",
+	SignatureMethod: "HMAC-SHA1",
+	Timestamp:       "1318622958",
+	AccessToken:     "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb",
+	Version:         "1.0",
+}
+
+var testBody = url.Values{
+	"status": []string{"Hello Ladies + Gentlemen, a signed OAuth request!"},
+}
+
+func init() {
+}
+
+func TestConfigHashKey(t *testing.T) {
+	key := testConfig.HashKey()
+	if key != expectedHashKey {
+		t.Errorf("Expected: %s\tGot: %s", expectedHashKey, key)
+	}
+}
+
+func TestOAuthSigning(t *testing.T) {
+	req, err := http.NewRequest("POST", "https://api.twitter.com/1/statuses/update.json?include_entities=true", bytes.NewBuffer([]byte(testBody.Encode())))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	params, err := sortedOauthParameters(req, &testHeader)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	sigString := oauthSignatureString(req, params)
+
+	if sigString != expectedSigString {
+		t.Errorf("\n\tExpected: %s\n\tGot: %s", expectedSigString, sigString)
+	}
+
+	signature := oauthSign(sigString, testConfig.HashKey())
+
+	if signature != expectedSignature {
+		t.Errorf("Expected: %s\tGot: %s", expectedSignature, signature)
+	}
+}
